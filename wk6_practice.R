cdc875207f78c8b460822b83ac2e569be66cad69
duccioa <duccio.aiazzi@gmail.com>
2015-11-19 16:03:35 +0000

    Quadrant analysis

--- wk6_practice.R
+++ wk6_practice.R
@@ -3,10 +3,10 @@ library(rgdal)
 library(rgeos)
 library(ggmap)
 library(sp)
-library(spatstat)
+library(spatstat)#spatial analysis tools
 library(GISTools)
 
-
+#add alpha to colours
 add.alpha <- function(col, alpha=1){
     if(missing(col))
         stop("Please provide a vector of colours.")
@@ -14,6 +14,8 @@ add.alpha <- function(col, alpha=1){
           function(x) 
               rgb(x[1], x[2], x[3], alpha=alpha))  
 }
+
+
 
 blueplaques <- readOGR("./Blue_plaques", "london_blueplaques")
 blueplaques <- spTransform(blueplaques, CRS("+init=epsg:27700"))
@@ -29,3 +31,19 @@ bp_sub <- blueplaques[boroughs,]
 
 plot(boroughs)
 points(bp_sub, pch = 20, lwd = 3, col = add.alpha("blue", 0.2))
+
+Harrow <- boroughs[boroughs@data$name == "Harrow",]
+bp_harrow <- bp_sub[Harrow,]
+plot(Harrow)
+points(bp_harrow, pch = 20, lwd = 3, col = add.alpha("blue", 0.5))
+#Spatial analysis with spatstat
+#Needs its own objects called ppp
+window <- as.owin(Harrow)
+bp.ppp <- ppp(x = bp_harrow@coords[,1], y = bp_harrow@coords[,2], window = window)
+plot(bp.ppp, pch = 20, cex = 2, col = add.alpha("blue", 0.4), main="Blue Plaques Harrow")
+
+#plot Kernel Density Estimation
+plot(density(bp.ppp, sigma = 500), main = "Kernel Density Estimation")
+#QUADRAT ANALYSIS
+plot(bp.ppp, pch = 20, cex = 1,  main="Blue Plaques Harrow")
+plot(quadratcount(bp.ppp, nx =6, ny = 6), col = "red", add = T)
